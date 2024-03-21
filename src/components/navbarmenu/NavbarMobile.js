ea17833ef2f062a61bf49e00a50e476bc040127b
Abdullah Arafa <abdullahciti@gmx.de>
2024-03-21 11:55:28 +0100

    Add .gitignore to ignore node_modules/

---
+++ src/components/navbarmenu/NavbarMobile.js
@@ -0,0 +1,126 @@
+import { Link } from "react-router-dom";
+// Icons
+import { MdDashboard } from "react-icons/md";
+import { FaUserFriends } from "react-icons/fa";
+import { IoIosSettings } from "react-icons/io";
+import { ImProfile } from "react-icons/im";
+import { FaProjectDiagram } from "react-icons/fa";
+import { SiCoursera } from "react-icons/si";
+import { LuFileSearch } from "react-icons/lu";
+import { MdOutlinePayments } from "react-icons/md";
+import handleActive from "../../functions/handleActive";
+
+
+const NavbarMobile = () => {
+    return ( 
+
+
+// {/* <PiBellRingingBold /> */}
+    <div className="navbar bg-white p-20 p-fixed w-50">
+      <Link className="link" to="/">
+        <h3 className="p-relative txt-c mt-0">TopC</h3>
+      </Link>
+      <ul>
+        <li>
+          <Link
+            onClick={(e) => handleActive(e, "ul li a")}
+            className="d-flex align-center fs-14 c-black rad-6 p-10"
+            to="/dashboard"
+          >
+            <i>
+              <MdDashboard />
+            </i>
+            <span>Dashboard</span>
+          </Link>
+        </li>
+        <li>
+          <Link
+            onClick={(e) => handleActive(e, "ul li a")}
+            className="d-flex align-center fs-14 c-black rad-6 p-10"
+            to="/settings"
+          >
+            <i>
+              <IoIosSettings />
+            </i>
+            <span>Settings</span>
+          </Link>
+        </li>
+        <li>
+          <Link
+            onClick={(e) => handleActive(e, "ul li a")}
+            className="d-flex align-center fs-14 c-black rad-6 p-10"
+            to="/profile"
+          >
+            <i>
+              <ImProfile />
+            </i>
+            <span>Profile</span>
+          </Link>
+        </li>
+        <li>
+          <Link
+            onClick={(e) => handleActive(e, "ul li a")}
+            className="d-flex align-center fs-14 c-black rad-6 p-10"
+            to="/projects"
+          >
+            <i>
+              <FaProjectDiagram />
+            </i>
+            <span>Projects</span>
+          </Link>
+        </li>
+        <li>
+          <Link
+            onClick={(e) => handleActive(e, "ul li a")}
+            className="d-flex align-center fs-14 c-black rad-6 p-10"
+            to="/courses"
+          >
+            <i>
+              <SiCoursera />
+            </i>
+            <span>Courses</span>
+          </Link>
+        </li>
+        <li>
+          <Link
+            onClick={(e) => handleActive(e, "ul li a")}
+            className="d-flex align-center fs-14 c-black rad-6 p-10"
+            to="/friends"
+          >
+            <i>
+              <FaUserFriends />
+            </i>
+            <span>Friends</span>
+          </Link>
+        </li>
+        <li>
+          <Link
+            onClick={(e) => handleActive(e, "ul li a")}
+            className="d-flex align-center fs-14 c-black rad-6 p-10"
+            to="/files"
+          >
+            <i>
+              <LuFileSearch />
+            </i>
+            <span>Files</span>
+          </Link>
+        </li>
+        <li>
+          <Link
+            onClick={(e) => handleActive(e, "ul li a")}
+            className="d-flex align-center fs-14 c-black rad-6 p-10"
+            to="/plans"
+          >
+            <i>
+              <MdOutlinePayments />
+            </i>
+            <span>Plans</span>
+          </Link>
+        </li>
+      </ul>
+    </div>
+
+     );
+}
+ 
+export default NavbarMobile;
