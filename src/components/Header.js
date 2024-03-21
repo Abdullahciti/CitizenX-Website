c9679e1c68c6c91eee2a055fb3d2af238d3d7037
Top C <136592513+Abdullahciti@users.noreply.github.com>
2024-03-21 12:01:59 +0100

    Add files via upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
 import { Avatar } from "../assets";
-import { PiBellRingingBold } from "react-icons/pi";
-import { FaSearch } from "react-icons/fa";
 
-const Header = () => {
+// Icons
+import { PiBellRingingBold } from "react-icons/pi";
+import { FaSearch, FaBars } from "react-icons/fa";
+const Header = ({ menu, handleMenu }) => {
   return (
-    <div className="head bg-white p-15 between-flex">
+    <div className="head bg-white p-15 between-flex p-relative">
+        <div className="menu p-absolute" onClick={handleMenu}>
+          <FaBars />
+        </div>
       <div className="search p-relative">
         <FaSearch />
         <input className="p-10" type="search" placeholder="Type A Keyword" />
